1493757b894b158abea09bedf350c037c572b4c5
윤서정 <sjyun0507@gmail.com>
2025-07-25 14:20:47 +0900

    Initial commit

---
+++ src/main/java/com/cafe_kiosk/kiosk_user/KioskUserApplication.java
@@ -0,0 +1,13 @@
+package com.cafe_kiosk.kiosk_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cafe_kiosk/kiosk_user/KioskUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe_kiosk.kiosk_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
